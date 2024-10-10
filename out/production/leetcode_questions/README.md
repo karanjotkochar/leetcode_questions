0fb5b211427ec473ebae6da781225763a81a05e8
karanjotkochar <karanjot.kochar@gmail.com>
2024-10-09 21:24:06 -0400

    add q_217

---
+++ out/production/leetcode_questions/README.md
@@ -0,0 +1,2 @@
+# leetcode_questions
+DSA Questions - LeetCode
--- q_217_Contains_Duplicate.java
+++ q_217_Contains_Duplicate.java
@@ -1,3 +1,46 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class q_217_Contains_Duplicate {
+    public static void main(String[] args) {
+        //containsDuplicate1();
+        //containsDuplicate2();
+        //containsDuplicate3();
+    }
+    public static boolean containsDuplicate1(int[] nums) {
+
+        for(int i=0; i<=nums.length-1; i++) {
+            int number = nums[i];
+            for (int j=i+1; j<=nums.length-1; j++) {
+                if (nums[j] == nums[i]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public static boolean containsDuplicate2(int[] nums) {
+
+        Arrays.sort(nums);
+        for(int i=0; i<nums.length-1; i++) {
+            if(nums[i] == nums[i+1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public static boolean containsDuplicate3(int[] nums) {
+
+        Set<Integer> set = new HashSet<>();
+
+        for(int i : nums) {
+            if(set.contains(i)){
+                return true;
+            }
+            set.add(i);
+        }
+        return false;
+    }
 
 }
