f4cca9d10e3baa31c367f0b257b70bc64e3c6bb2
karanjotkochar <karanjot.kochar@gmail.com>
2024-10-13 13:20:28 -0400

    add q_283 v2

--- q_283_Move_Zeroes.java
+++ q_283_Move_Zeroes.java
@@ -4,8 +4,8 @@ import java.util.List;
 public class q_283_Move_Zeroes {
 //    https://leetcode.com/problems/move-zeroes/description/
     public static void main(String[] args) {
-        int[] nums  = {0,1,0,3,12};
-        moveZeroes(nums);
+//        moveZeroes();
+//        moveZeroes2();
     }
     public static void moveZeroes(int[] nums) {
 
@@ -23,6 +23,24 @@ public class q_283_Move_Zeroes {
         }
 
         System.out.println(result);
+    }
+
+    public static void moveZeroes2(int[] nums) {
+
+        // Two Pointer approach
+        int j = 0;
+        int temp = 0;
+        int n = nums.length;
+
+        for(int i=0; i<n; i++) {
+            if(nums[i] != 0) {
+                temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;
+            }
+        }
+
     }
 
 }
