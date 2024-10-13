b7d272391366dda23799419db9453b9ce3fca495
karanjotkochar <karanjot.kochar@gmail.com>
2024-10-13 15:25:53 -0400

    add q_21 v2

--- q_21_Merge_Two_Sorted_Lists.java
+++ q_21_Merge_Two_Sorted_Lists.java
@@ -17,6 +17,7 @@ class ListNode {
 }
 
 public class q_21_Merge_Two_Sorted_Lists {
+
     public static void main(String[] args) {
 
         ListNode list1 = new ListNode(1);
@@ -44,24 +45,26 @@ public class q_21_Merge_Two_Sorted_Lists {
      * @return
      */
 
-
-
     public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
 
         // Recusrsion, LinkedList
         if (list1 != null && list2 != null) {
+
             if (list1.val < list2.val) {
                 list1.next = mergeTwoLists(list1.next, list2);
                 return list1;
+
             } else {
                 list2.next = mergeTwoLists(list1, list2.next);
                 return list2;
+
             }
         } else if (list1 == null) {
             return list2;
 
         } else {
             return list1;
+
         }
     }
 
