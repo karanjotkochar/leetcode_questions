a4dd681649fcfa9f0f60f591b18a885e08fb88f5
karanjotkochar <karanjot.kochar@gmail.com>
2024-10-14 13:19:24 -0400

    add  q_1 v2

--- q_1_Two_Sum.java
+++ q_1_Two_Sum.java
@@ -1,10 +1,17 @@
 //https://leetcode.com/problems/two-sum/description/
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class q_1_Two_Sum {
     public static void main(String[] args) {
-        int[] nums = {3,3};
+        int[] nums = {3,2,4};
         int target = 6;
-        twoSum1(nums, target);
+//        twoSum1(nums, target);
+//        twoSum2(nums, target);
+//        twoSum3(nums, target);
+
     }
 
     public static int[] twoSum1(int[] nums, int target) {
@@ -25,4 +32,47 @@ public class q_1_Two_Sum {
 
         return finalArray;
     }
+
+    public static int[] twoSum2(int[] nums, int target) {
+
+        // Two Pointer Approach but alters the order due to sorting
+        int left = 0;
+        int right = (nums.length-1);
+
+        Arrays.sort(nums);
+
+        while(left < right) {
+            int sum = nums[left] + nums[right];
+
+            if(sum < target) {
+                left++;
+            } else if (sum > target) {
+                right--;
+            } else {
+                return new int[] {left, right};
+            }
+        }
+
+        return new int[] {};
+    }
+
+    public static boolean twoSum3(int[] nums, int target) {
+
+        // Using HashSet, returns boolean result
+        Set<Integer> set = new HashSet<>();
+        int n = nums.length;
+
+        for (int num : nums) {
+            set.add(num);
+        }
+
+        for(int i=0; i<n; i++) {
+            int complement = target - nums[i];
+            if (set.contains(complement)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
