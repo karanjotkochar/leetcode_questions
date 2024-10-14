25450957768e4137f4c4ab17d2a25a8aee1b1486
karanjotkochar <karanjot.kochar@gmail.com>
2024-10-14 13:44:57 -0400

    add q_977 v2

--- q_977_Squares_Sorted_Array.java
+++ q_977_Squares_Sorted_Array.java
@@ -5,10 +5,11 @@ import java.util.Arrays;
 public class q_977_Squares_Sorted_Array {
     public static void main(String[] args) {
         int[] nums = {-4,-1,0,3,10};
-        sortedSquares(nums);
+//        sortedSquares1(nums);
+//        sortedSquares2(nums);
     }
 
-    public static int[] sortedSquares(int[] nums) {
+    public static int[] sortedSquares1(int[] nums) {
 
         // Brute force approach
         int n = nums.length;
@@ -18,6 +19,27 @@ public class q_977_Squares_Sorted_Array {
         Arrays.sort(nums);
 
         return nums;
+    }
+
+    public static int[] sortedSquares2(int[] nums) {
+
+        // Two pointer approach
+        int n = nums.length;
+        int left = 0;
+        int right = n - 1;
+        int[] result = new int[n];
+
+        for (int i = n-1; i>=0; i--) {
+            if (Math.abs(nums[left]) > Math.abs(nums[right])) {
+                result[i] = nums[left] * nums[left];
+                left++;
+            } else {
+                result[i] = nums[right] * nums[right];
+                right--;
+            }
+        }
+
+        return result;
     }
 
 }
