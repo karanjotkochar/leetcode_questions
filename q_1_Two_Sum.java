908c21a13226e765d1a2adffc12547c292f8f41d
karanjotkochar <karanjot.kochar@gmail.com>
2024-10-14 12:39:37 -0400

    add  q_1 v1

--- q_1_Two_Sum.java
+++ q_1_Two_Sum.java
@@ -1,4 +1,28 @@
 //https://leetcode.com/problems/two-sum/description/
 
 public class q_1_Two_Sum {
+    public static void main(String[] args) {
+        int[] nums = {3,3};
+        int target = 6;
+        twoSum1(nums, target);
+    }
+
+    public static int[] twoSum1(int[] nums, int target) {
+
+        // Brute force approach
+        int[] finalArray = new int[2];
+        int n = nums.length;
+
+        for (int i=0; i<n; i++) {
+           for (int j=i+1; j<n; j++) {
+               if(nums[i] + nums[j] == target) {
+                   finalArray[0] = i;
+                   finalArray[1] = j;
+                   return finalArray;
+               }
+           }
+        }
+
+        return finalArray;
+    }
 }
