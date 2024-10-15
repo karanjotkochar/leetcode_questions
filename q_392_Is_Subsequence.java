63b66f507fc62cd5f538d27b5eded775994ec9a5
karanjotkochar <karanjot.kochar@gmail.com>
2024-10-14 20:53:01 -0400

    add  q_392 v1: draft

--- q_392_Is_Subsequence.java
+++ q_392_Is_Subsequence.java
@@ -2,6 +2,32 @@
 
 public class q_392_Is_Subsequence {
     public static void main(String[] args) {
+        String s = "ab";
+        String t = "baab";
+        isSubsequence(s,t);
+        System.out.println(isSubsequence(s,t));
 
+    }
+
+    public static boolean isSubsequence(String s, String t) {
+        int sLength = s.length();
+        int tLength = t.length();
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0; i<tLength; i++) {
+            char chT = t.charAt(i);
+            for (int j=0; j<sLength; j++) {
+                char chS = s.charAt(j);
+
+                if(chS == chT){
+                    sb.append(chS);
+                    break;
+                }
+
+            }
+        }
+
+        return sb.toString().equals(s);
     }
 }
