a9fc708af5620b695704665d138aa2f3bf0f801e
karanjotkochar <karanjot.kochar@gmail.com>
2024-10-14 20:53:01 -0400

    add  q_392 v1

--- q_392_Is_Subsequence.java
+++ q_392_Is_Subsequence.java
@@ -2,6 +2,23 @@
 
 public class q_392_Is_Subsequence {
     public static void main(String[] args) {
+//        isSubsequence(s,t);
+    }
 
+    public static boolean isSubsequence(String s, String t) {
+
+        // Two Pointer approach
+        int sPointer = 0;
+        int tPointer = 0;
+
+        while(sPointer<s.length() && tPointer<t.length()) {
+
+            if(s.charAt(sPointer) == t.charAt(tPointer)) {
+                sPointer++;
+            }
+            tPointer++;
+        }
+
+        return (sPointer == s.length());
     }
 }
