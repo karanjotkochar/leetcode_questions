42251df27ee982dd8ff0b648a05086616826d583
karanjotkochar <karanjot.kochar@gmail.com>
2024-10-16 23:23:53 -0400

    add q_643 v1

--- q_643_Max_Avg_Subarray.java
+++ q_643_Max_Avg_Subarray.java
@@ -1,7 +1,37 @@
 //https://leetcode.com/problems/maximum-average-subarray-i/description/
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class q_643_Max_Avg_Subarray {
     public static void main(String[] args) {
+        int[] nums = {1,12,-5,-6,50,3};
+        int k = 4;
+        findMaxAverage1(nums, k);
+        System.out.println(findMaxAverage1(nums, k));
+    }
+    public static double findMaxAverage1(int[] nums, int k) {
 
+        // Brute force approach
+        int n = nums.length;
+        double max = 0.0;
+
+        for(int i=0; i<n; i++) {
+
+            double finalSum = 0.0;
+            int count = 0;
+
+            for(int j = i; j<i+k&&j<n; j++){
+                finalSum = finalSum + nums[j];
+                count++;
+            }
+
+            if(count == k){
+                double avg = finalSum/k;
+                max = Math.max(avg, max);
+            }
+        }
+
+        return max;
     }
 }
