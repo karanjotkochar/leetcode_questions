199b8cb3a1a715c70412f3c5b1270b511d7c6db4
karanjotkochar <karanjot.kochar@gmail.com>
2024-10-20 22:12:13 -0400

    draft

--- q_70_Climbing_Stairs.java
+++ q_70_Climbing_Stairs.java
@@ -2,6 +2,13 @@
 
 public class q_70_Climbing_Stairs {
     public static void main(String[] args) {
+        int n=2;
+        climbStairs(n);
+        System.out.println(climbStairs(n));
+    }
+    public static int climbStairs(int n) {
 
+
+        return n;
     }
 }
