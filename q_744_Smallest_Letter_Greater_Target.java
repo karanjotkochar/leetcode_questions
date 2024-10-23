25d71ab8de2d0d336a1beac952b3a1f4fa0d5c54
karanjotkochar <karanjot.kochar@gmail.com>
2024-10-22 23:59:54 -0400

    draft q_744 v1

--- q_744_Smallest_Letter_Greater_Target.java
+++ q_744_Smallest_Letter_Greater_Target.java
@@ -2,6 +2,26 @@
 
 public class q_744_Smallest_Letter_Greater_Target {
     public static void main(String[] args) {
+       char[] letters = {'c', 'f', 'j'};
+       char target = 'a';
+
+       nextGreatestLetter(letters, target);
+    }
+
+    public static char nextGreatestLetter(char[] letters, char target) {
+        int start =0;
+        int end= letters.length-1;
+        while(start<=end){
+            int mid = start+(end-start)/2;
+
+            if(letters[mid]>target){
+                end = mid-1;
+            }else{
+                start = mid+1;
+            }
+
+        }
+        return letters[start % letters.length];
 
     }
 }
