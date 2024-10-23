fa1e69c64d9936efba032ad67a208d012fc4e704
karanjotkochar <karanjot.kochar@gmail.com>
2024-10-22 23:29:39 -0400

    begin q_744

---
+++ q_744_Smallest_Letter_Greater_Target.java
@@ -0,0 +1,7 @@
+//https://leetcode.com/problems/find-smallest-letter-greater-than-target/description/
+
+public class q_744_Smallest_Letter_Greater_Target {
+    public static void main(String[] args) {
+
+    }
+}
