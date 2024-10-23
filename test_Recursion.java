ea60d0a103656a4b26b0f979fe4143b7ade54c33
karanjotkochar <karanjot.kochar@gmail.com>
2024-10-22 21:28:33 -0400

    recursion v2

--- test_Recursion.java
+++ test_Recursion.java
@@ -1,18 +1,27 @@
 public class test_Recursion {
     public static void main(String[] args) {
-        int n=5;
-        recursiveFunction1(n);
-        System.out.println(recursiveFunction1(n));
+        int n = 5;
+//        recursiveFunction1(n);
+//        System.out.println(recursiveFunction1(n));
+
+        recursiveFunction2(n);
+        System.out.println(recursiveFunction2(n));
     }
 
     public static int recursiveFunction1(int n) {
-        int sum = 0;
-
         // Sum of n digits
-        for(int i=1; i<=n; i++) {
-            sum = i + recursiveFunction1(i-1);
-
+        if (n==0){
+            return 0;
         }
-        return sum;
+
+        return n + recursiveFunction1(n-1);
+    }
+
+    public static int recursiveFunction2(int n) {
+        // Factorial
+        if (n==0)
+            return 1;
+
+        return n * recursiveFunction2(n-1);
     }
 }
