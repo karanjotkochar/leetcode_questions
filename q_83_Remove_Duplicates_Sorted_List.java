244afe3ea37ae2dff9029a341e90d3faec571c6d
karanjotkochar <karanjot.kochar@gmail.com>
2024-10-27 19:57:25 -0400

    draft: add q_83 v1

--- q_83_Remove_Duplicates_Sorted_List.java
+++ q_83_Remove_Duplicates_Sorted_List.java
@@ -1,7 +1,40 @@
 // https://leetcode.com/problems/remove-duplicates-from-sorted-list/description/
-
+class ListNode6 {
+    int val;
+    ListNode6 next;
+    ListNode6() {}
+    ListNode6(int val) {
+        this.val = val;
+    }
+    ListNode6(int val, ListNode6 next) {
+        this.val = val;
+        this.next = next;
+    }
+}
 public class q_83_Remove_Duplicates_Sorted_List {
     public static void main(String[] args) {
 
+        ListNode6 listNode = new ListNode6(1);
+        listNode.next = new ListNode6(1);
+        listNode.next.next = new ListNode6(2);
+        listNode.next.next.next = new ListNode6(3);
+        listNode.next.next.next.next = new ListNode6(3);
+
+//        deleteDuplicates(listNode);
+        printLL(listNode);
+
+    }
+
+    public static ListNode6 deleteDuplicates(ListNode6 head) {
+
+        return head;
+    }
+
+    public static void printLL(ListNode6 head) {
+
+        while (head != null) {
+            System.out.print(" " + head.val);
+            head = head.next;
+        }
     }
 }
