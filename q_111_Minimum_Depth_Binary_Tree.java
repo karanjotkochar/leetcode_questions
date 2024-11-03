646751f74dc528ad9f36105b6bd00df10af703d9
karanjotkochar <karanjot.kochar@gmail.com>
2024-11-03 17:04:02 -0500

    begin q_111 draft v2

--- q_111_Minimum_Depth_Binary_Tree.java
+++ q_111_Minimum_Depth_Binary_Tree.java
@@ -1,7 +1,24 @@
 // https://leetcode.com/problems/minimum-depth-of-binary-tree/description/
-
+class TreeNode2 {
+    int val;
+    TreeNode2 left;
+    TreeNode2 right;
+    TreeNode2() {}
+    TreeNode2(int val) {
+        this.val = val;
+    }
+    TreeNode2(int val, TreeNode2 left, TreeNode2 right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
 public class q_111_Minimum_Depth_Binary_Tree {
     public static void main(String[] args) {
 
+    }
+    public static int minDepth(TreeNode2 root) {
+
+        return 0;
     }
 }
--- q_637_Average_Levels_Binary_Tree.java
+++ q_637_Average_Levels_Binary_Tree.java
@@ -3,13 +3,13 @@ import java.util.List;
 // https://leetcode.com/problems/average-of-levels-in-binary-tree/description/
 class TreeNode {
     int val;
-    TreeNode left;
-    TreeNode right;
+    TreeNode2 left;
+    TreeNode2 right;
     TreeNode() {}
     TreeNode(int val) {
         this.val = val;
     }
-    TreeNode(int val, TreeNode left, TreeNode right) {
+    TreeNode(int val, TreeNode2 left, TreeNode2 right) {
         this.val = val;
         this.left = left;
         this.right = right;
@@ -17,14 +17,14 @@ class TreeNode {
 }
 public class q_637_Average_Levels_Binary_Tree {
     public static void main(String[] args) {
-        TreeNode treeNode = new TreeNode(3);
+        TreeNode2 treeNode = new TreeNode2(3);
 
         averageOfLevels(treeNode);
 
 
     }
 
-    public static List<Double> averageOfLevels(TreeNode root) {
+    public static List<Double> averageOfLevels(TreeNode2 root) {
 
         return null;
     }
