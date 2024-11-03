a7d078c43e2bd04f59a456ccdef7d45002c04789
karanjotkochar <karanjot.kochar@gmail.com>
2024-11-03 16:56:39 -0500

    begin q_637 draft v1

--- q_637_Average_Levels_Binary_Tree.java
+++ q_637_Average_Levels_Binary_Tree.java
@@ -1,2 +1,7 @@
+// https://leetcode.com/problems/average-of-levels-in-binary-tree/description/
+
 public class q_637_Average_Levels_Binary_Tree {
+    public static void main(String[] args) {
+
+    }
 }
