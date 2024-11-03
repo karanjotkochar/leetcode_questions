7b8477974195f9f92c3b8fcb3d41196e5dbfd92e
karanjotkochar <karanjot.kochar@gmail.com>
2024-11-03 17:02:38 -0500

    begin q_111 draft v1

---
+++ q_111_Minimum_Depth_Binary_Tree.java
@@ -0,0 +1,7 @@
+// https://leetcode.com/problems/minimum-depth-of-binary-tree/description/
+
+public class q_111_Minimum_Depth_Binary_Tree {
+    public static void main(String[] args) {
+
+    }
+}
