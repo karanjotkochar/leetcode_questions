0152e4432524b1f3fef2d3b93ccb3c155041ebb2
karanjotkochar <karanjot.kochar@gmail.com>
2024-11-03 17:01:26 -0500

    begin q_637 draft v2

--- q_637_Average_Levels_Binary_Tree.java
+++ q_637_Average_Levels_Binary_Tree.java
@@ -1,7 +1,31 @@
-// https://leetcode.com/problems/average-of-levels-in-binary-tree/description/
+import java.util.List;
 
+// https://leetcode.com/problems/average-of-levels-in-binary-tree/description/
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) {
+        this.val = val;
+    }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
 public class q_637_Average_Levels_Binary_Tree {
     public static void main(String[] args) {
+        TreeNode treeNode = new TreeNode(3);
 
+        averageOfLevels(treeNode);
+
+
+    }
+
+    public static List<Double> averageOfLevels(TreeNode root) {
+
+        return null;
     }
 }
