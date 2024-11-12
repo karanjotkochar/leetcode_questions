0cd866c2261db6ab4fd04712b2d79afe5272f31a
karanjotkochar <karanjot.kochar@gmail.com>
2024-11-11 23:59:36 -0500

    add test/stack: array deque implementation

--- test_stack_ds.java
+++ test_stack_ds.java
@@ -1,11 +1,14 @@
+import java.util.ArrayDeque;
 import java.util.Stack;
 
 public class test_stack_ds {
     public static void main(String[] args) {
         vectorImplementation();
+        arrayDequeImplementation();
     }
 
     public static void vectorImplementation() {
+
         // Vector implementation
         Stack<Integer> stack = new Stack<>();
         // push(), pop(), top(), isEmpty(), size()
@@ -22,6 +25,26 @@ public class test_stack_ds {
         while (!stack.empty()) {
             System.out.print(stack.pop() + " ");
         }
+
+    }
+
+    public static void arrayDequeImplementation() {
+
+        // Array Deque implementation
+
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+
+        System.out.println("size: " + stack.size());
+        System.out.println("peek/top: " + stack.peek());
+
+        while (!stack.isEmpty()) {
+            System.out.print(stack.pop() + " ");
+        }
+
 
     }
 
