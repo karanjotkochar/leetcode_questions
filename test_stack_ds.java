c315740d5f03fe0df5b54de8051844b6a8ad69fd
karanjotkochar <karanjot.kochar@gmail.com>
2024-11-11 23:52:03 -0500

    add test/stack: vector implementation

--- test_stack_ds.java
+++ test_stack_ds.java
@@ -1,3 +1,28 @@
+import java.util.Stack;
+
 public class test_stack_ds {
+    public static void main(String[] args) {
+        vectorImplementation();
+    }
+
+    public static void vectorImplementation() {
+        // Vector implementation
+        Stack<Integer> stack = new Stack<>();
+        // push(), pop(), top(), isEmpty(), size()
+
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+
+        int size = stack.size();
+        System.out.println("size: " + size);
+
+        System.out.println("peek/top: " + stack.peek());
+
+        while (!stack.empty()) {
+            System.out.print(stack.pop() + " ");
+        }
+
+    }
 
 }
