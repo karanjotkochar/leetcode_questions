adf6609c266441b7a7c33d76b2d4d99ff716c4ec
karanjotkochar <karanjot.kochar@gmail.com>
2024-11-13 23:47:36 -0500

    add draft: q_100 v1

--- q_100_Same_Tree.java
+++ q_100_Same_Tree.java
@@ -1,4 +1,20 @@
 // https://leetcode.com/problems/same-tree/description/
+
+class TreeNode3 {
+    int val;
+    TreeNode3 left;
+    TreeNode3 right;
+    TreeNode3() {};
+    TreeNode3(int val) {
+        this.val = val;
+    }
+    TreeNode3(int val, TreeNode3 left, TreeNode3 right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+}
 public class q_100_Same_Tree {
     public static void main(String[] args) {
 
