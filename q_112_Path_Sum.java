a7e3bb60f3d8c5a48a78e4eec354fd1b1303734f
karanjotkochar <karanjot.kochar@gmail.com>
2024-11-14 00:12:11 -0500

    draft: q_112 v1

--- q_112_Path_Sum.java
+++ q_112_Path_Sum.java
@@ -1,5 +1,18 @@
 // https://leetcode.com/problems/path-sum/description/
-
+class TreeNode4 {
+    int val;
+    TreeNode4 left;
+    TreeNode4 right;
+    TreeNode4() {}
+    TreeNode4(int val) {
+        this.val = val;
+    }
+    TreeNode4(int val, TreeNode4 left, TreeNode4 right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
 public class q_112_Path_Sum {
     public static void main(String[] args) {
 
