b113e70f7104eb2faab8a8ac4131971e50a3ec98
karanjotkochar <karanjot.kochar@gmail.com>
2024-11-13 23:51:23 -0500

    add draft: q_100 v2

--- q_100_Same_Tree.java
+++ q_100_Same_Tree.java
@@ -17,6 +17,23 @@ class TreeNode3 {
 }
 public class q_100_Same_Tree {
     public static void main(String[] args) {
+        TreeNode3 p = null;
+        p = new TreeNode3(1);
+        p.left = new TreeNode3(2);
+        p.right = new TreeNode3(3);
 
+        TreeNode3 q = null;
+        q = new TreeNode3(1);
+        q.left = new TreeNode3(2);
+        q.right = new TreeNode3(3);
+
+        Boolean val = isSameTree(p, q);
+        System.out.println(val);
+
+    }
+
+    public static boolean isSameTree(TreeNode3 p, TreeNode3 q) {
+
+    return false;
     }
 }
