e30d85e180969548725107c527762820d89ef5ae
karanjotkochar <karanjot.kochar@gmail.com>
2024-11-13 23:58:25 -0500

    add: q_100 v1

--- q_100_Same_Tree.java
+++ q_100_Same_Tree.java
@@ -34,6 +34,15 @@ public class q_100_Same_Tree {
 
     public static boolean isSameTree(TreeNode3 p, TreeNode3 q) {
 
+        if (p == null && q == null)
+            return true;
+
+        if (p == null || q == null)
+            return false;
+
+        if (p.val == q.val && (isSameTree(p.left, q.left)) && isSameTree(p.right, q.right))
+            return true;
+
     return false;
     }
 }
