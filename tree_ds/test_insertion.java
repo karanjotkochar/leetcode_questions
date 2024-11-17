ad9a9240b38dfb0f180f863318042dfec0b78274
karanjotkochar <karanjot.kochar@gmail.com>
2024-11-17 18:09:00 -0500

    add: insert@Binary tree

--- tree_ds/test_insertion.java
+++ tree_ds/test_insertion.java
@@ -1,5 +1,8 @@
 package tree_ds;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class test_insertion {
 
     public static void main(String[] args) {
@@ -10,12 +13,58 @@ public class test_insertion {
 
         int key = 6;
 
-        insertElement(root, key);
+        inorderTraversal(root);
+        System.out.println();
+
+        BinaryTreeNode newRoot = insertElement(root, key);
+        inorderTraversal(newRoot);
+
     }
 
-    public static void insertElement(BinaryTreeNode root, int key) {
+    public static BinaryTreeNode insertElement(BinaryTreeNode root, int key) {
 
+        // Empty Tree
+        if (root == null)
+            return new BinaryTreeNode(key);
 
+        // Use BFS/Level order traversal
+        Queue<BinaryTreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            BinaryTreeNode temp = queue.poll();
+
+            if (temp.left == null) {
+                // If left is Empty, add there
+                temp.left = new BinaryTreeNode(key);
+                break;
+
+            } else {
+                // If left is present, add to queue
+                queue.add(temp.left);
+            }
+
+            if (temp.right == null) {
+                // If right is Empty, add there
+                temp.right = new BinaryTreeNode(key);
+                break;
+
+            } else {
+                // If right is present, add to queue
+                queue.add(temp.right);
+            }
+
+        }
+
+        return root;
+    }
+
+    public static void inorderTraversal(BinaryTreeNode root) {
+        if (root == null)
+            return;
+        inorderTraversal(root.left);
+        System.out.print(root.data + " ");
+        inorderTraversal(root.right);
     }
 
 }
