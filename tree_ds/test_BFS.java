f0a9c131894fe98b2df51a2285ac237ec06781d8
karanjotkochar <karanjot.kochar@gmail.com>
2024-11-17 17:42:02 -0500

    refactor: BFS v2

--- tree_ds/test_BFS.java
+++ tree_ds/test_BFS.java
@@ -15,7 +15,7 @@ class Node3 {
     }
 }
 
-public class test_Binary_Tree_Traversal_BFS {
+public class test_BFS {
     public static void main(String[] args) {
         Node3 root = new Node3(1);
             root.left = new Node3(2);
