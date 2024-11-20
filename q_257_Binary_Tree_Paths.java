83a1a09cfbabb8b5c717c831fb0419e9d3dbedac
karanjotkochar <karanjot.kochar@gmail.com>
2024-11-20 09:50:49 -0500

    begin: q_257: TO DO

--- q_257_Binary_Tree_Paths.java
+++ q_257_Binary_Tree_Paths.java
@@ -1,5 +1,6 @@
 // https://leetcode.com/problems/binary-tree-paths/description/
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class q_257_Binary_Tree_Paths {
@@ -7,15 +8,29 @@ public class q_257_Binary_Tree_Paths {
         BinaryTreeNode root = new BinaryTreeNode(1);
         root.left = new BinaryTreeNode(2);
         root.right = new BinaryTreeNode(3);
-        root.left.left = new BinaryTreeNode(4);
         root.left.right = new BinaryTreeNode(5);
 
         binaryTreePaths(root);
     }
-
     public static List<String> binaryTreePaths(BinaryTreeNode root) {
+        ArrayList<String> list = new ArrayList<>();
 
+        if (root != null)
+            searchBT(root, "", list);
 
-        return null;
+        return list;
     }
+    public static void searchBT(BinaryTreeNode root, String path, ArrayList<String> list){
+
+        if (root.left == null && root.right == null)
+            aa;
+
+        if (root.left != null)
+            aa;
+
+        if (root.right != null)
+            aa;
+
+    }
+
 }
