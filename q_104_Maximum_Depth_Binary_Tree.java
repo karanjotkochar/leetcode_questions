a23941e261769e5a2bf13a7281c0343e88b4e8b1
karanjotkochar <karanjot.kochar@gmail.com>
2024-11-20 08:08:04 -0500

    q_104 v1

--- q_104_Maximum_Depth_Binary_Tree.java
+++ q_104_Maximum_Depth_Binary_Tree.java
@@ -1,5 +1,8 @@
 // https://leetcode.com/problems/maximum-depth-of-binary-tree/description/
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class q_104_Maximum_Depth_Binary_Tree {
     public static void main(String[] args) {
         BinaryTreeNode root = new BinaryTreeNode(3);
@@ -9,9 +12,38 @@ public class q_104_Maximum_Depth_Binary_Tree {
         root.right.right = new BinaryTreeNode(7);
 
         maxDepth(root);
+        System.out.println(maxDepth(root));
     }
     public static int maxDepth(BinaryTreeNode root) {
 
-        return 0;
+        // Edge case: Tree is empty
+        if (root == null)
+            return 0;
+
+        Queue<BinaryTreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        queue.add(null);
+
+        int count = 0;
+
+        while (!queue.isEmpty()) {
+            BinaryTreeNode current = queue.poll();
+
+            if (current == null) {
+                count++;
+                if (!queue.isEmpty()) {
+                    queue.add(null);
+                }
+            } else {
+                if (current.left != null)
+                    queue.add(current.left);
+
+                if (current.right != null)
+                    queue.add(current.right);
+
+            }
+        }
+
+        return count;
     }
 }
