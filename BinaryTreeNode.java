3750c621e19e7633da256d6c0c2b1978cfe90504
karanjotkochar <karanjot.kochar@gmail.com>
2024-11-20 07:45:23 -0500

    add binary tree class

---
+++ BinaryTreeNode.java
@@ -0,0 +1,11 @@
+public class BinaryTreeNode {
+    int data;
+    tree_ds.BinaryTreeNode left;
+    tree_ds.BinaryTreeNode right;
+
+    public BinaryTreeNode (int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
