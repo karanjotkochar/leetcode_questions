b5f16df7b7e85c635244b511c3dd5e0f287181ed
karanjotkochar <karanjot.kochar@gmail.com>
2024-11-20 08:26:00 -0500

    draft: q_543 v1

--- q_543_Diameter_Binary_Tree.java
+++ q_543_Diameter_Binary_Tree.java
@@ -2,6 +2,18 @@
 
 public class q_543_Diameter_Binary_Tree {
     public static void main(String[] args) {
+        BinaryTreeNode root = new BinaryTreeNode(1);
+        root.left = new BinaryTreeNode(2);
+        root.right = new BinaryTreeNode(3);
+        root.left.left = new BinaryTreeNode(4);
+        root.left.right = new BinaryTreeNode(5);
 
+        diameterOfBinaryTree(root);
+        System.out.println(diameterOfBinaryTree(root));
+
+    }
+    public static int diameterOfBinaryTree(BinaryTreeNode root) {
+
+        return 0;
     }
 }
