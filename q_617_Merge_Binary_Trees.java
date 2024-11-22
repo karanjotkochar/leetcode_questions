b92070b0432095833cc9d6022c30b8847672e4b0
karanjotkochar <karanjot.kochar@gmail.com>
2024-11-22 07:56:06 -0500

    draft: q_617 v1

--- q_617_Merge_Binary_Trees.java
+++ q_617_Merge_Binary_Trees.java
@@ -2,6 +2,22 @@
 
 public class q_617_Merge_Binary_Trees {
     public static void main(String[] args) {
+        BinaryTreeNode root1 = new BinaryTreeNode(1);
+        root1.left = new BinaryTreeNode(3);
+        root1.right = new BinaryTreeNode(2);
+        root1.right.left = new BinaryTreeNode(5);
 
+        BinaryTreeNode root2 = new BinaryTreeNode(2);
+        root2.left = new BinaryTreeNode(1);
+        root2.right = new BinaryTreeNode(3);
+        root2.left.right = new BinaryTreeNode(4);
+        root2.right.right = new BinaryTreeNode(7);
+
+        mergeTrees(root1, root2);
+    }
+
+    public static BinaryTreeNode mergeTrees(BinaryTreeNode root1, BinaryTreeNode root2) {
+
+        return null;
     }
 }
