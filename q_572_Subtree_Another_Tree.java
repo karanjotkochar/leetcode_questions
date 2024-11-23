edb2fb0c046aa614e407ef07997e8b5b38858804
karanjotkochar <karanjot.kochar@gmail.com>
2024-11-23 11:05:26 -0500

    draft: q_572 v1

--- q_572_Subtree_Another_Tree.java
+++ q_572_Subtree_Another_Tree.java
@@ -2,6 +2,21 @@
 
 public class q_572_Subtree_Another_Tree {
     public static void main(String[] args) {
+        BinaryTreeNode root = new BinaryTreeNode(3);
+        root.left = new BinaryTreeNode(4);
+        root.right = new BinaryTreeNode(5);
+        root.left.left = new BinaryTreeNode(1);
+        root.left.right = new BinaryTreeNode(2);
 
+        BinaryTreeNode subRoot = new BinaryTreeNode(4);
+        subRoot.left = new BinaryTreeNode(1);
+        subRoot.right = new BinaryTreeNode(2);
+
+        isSubtree(root, subRoot);
+
+    }
+    public static boolean isSubtree(BinaryTreeNode root, BinaryTreeNode subRoot) {
+
+        return false;
     }
 }
