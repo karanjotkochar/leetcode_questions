792885bffaeac905d43251f8efcc008ef33ee55d
karanjotkochar <karanjot.kochar@gmail.com>
2024-12-06 09:36:59 -0500

    add: q_252

--- q_252_Meeting_Rooms.java
+++ q_252_Meeting_Rooms.java
@@ -14,9 +14,7 @@ Input: intervals = [(5,8),(9,15)]
 Output: true
  */
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 class Interval {
     public int start, end;
@@ -38,20 +36,45 @@ public class q_252_Meeting_Rooms {
 
         canAttendMeetings(ls);
         System.out.println(canAttendMeetings(ls));
+
+        canAttendMeetings2(ls);
+        System.out.println(canAttendMeetings2(ls));
     }
 
     public static boolean canAttendMeetings(List<Interval> intervals) {
 
         int n = intervals.size();
+
         for (int i=0; i<n; i++) {
+            // get first element
             Interval A = intervals.get(i);
 
             for (int j= i+1; j<n; j++) {
+                // get second element
                 Interval B = intervals.get(j);
 
+                // check if first element ends after first would have started--> thus false
                 if (Math.min(A.end, B.end) > Math.max(A.start, B.start))
                     return false;
             }
+        }
+
+        return true;
+    }
+
+    public static boolean canAttendMeetings2(List<Interval> intervals) {
+
+        // sort based on start time
+        Collections.sort(intervals, Comparator.comparingInt(i -> i.start));
+
+        for (int i=1; i<intervals.size(); i++) {
+            // to sort 1st and 2nd: get them: i and i-1
+            // if i1 ends after i2 have started: cannot attend i.e., false
+            Interval i1 = intervals.get(i-1);
+            Interval i2 = intervals.get(i);
+
+            if (i1.end > i2.start)
+                return false;
         }
 
         return true;
