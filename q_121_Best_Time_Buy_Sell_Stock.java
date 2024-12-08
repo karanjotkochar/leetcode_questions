2aaa966c7e145d0fbd592fde7ac187211fbeffa4
karanjotkochar <karanjot.kochar@gmail.com>
2024-12-08 10:53:26 -0500

    add: q_121 draft v1

---
+++ q_121_Best_Time_Buy_Sell_Stock.java
@@ -0,0 +1,15 @@
+// Best Time to Buy and Sell Stock
+
+public class q_121_Best_Time_Buy_Sell_Stock {
+
+    public static void main(String[] args) {
+
+        int[] prices = {7,1,5,3,6,4};
+        maxProfit(prices);
+    }
+
+    public static int maxProfit(int[] prices) {
+
+        return 0;
+    }
+}
